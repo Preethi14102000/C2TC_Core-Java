2869baee86ce7d87c3c7344c158716c019bbd14c
Preethi0112 <106079232+Preethi0112@users.noreply.github.com>
2022-07-15 13:04:00 +0530

    *firstcommit

---
+++ ClassesandObjects/src/com/cg/classesandobjects/cricketer.java
@@ -0,0 +1,20 @@
+package com.cg.classesandobjects;
+
+public class cricketer {
+
+	public static void main(String[] args) {
+
+		class Cricketer{
+   //Instance variables
+String name ="Dhoni";
+int age = 39;
+String color ="Brown";
+String role ="Batsman";
+String nationality ="Indian";}}
+void Display(){
+System.out.println("I LOVE Cricket");
+}//end of method
+
+
+	
+}
