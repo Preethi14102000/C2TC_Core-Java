ee6c5f2729797fd8521afbf857fe21a4dfed2ce5
Preethi0112 <106079232+Preethi0112@users.noreply.github.com>
2022-07-15 11:52:48 +0530

    *firstcommit

---
+++ Loops/src/com/cg/loops/main.java
@@ -0,0 +1,15 @@
+package com.cg.loops;
+
+public class main {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i++)
+		 {
+		 System.out.println("Hello");
+		          i++;
+		      }    
+
+
+	}
+
+}
---
+++ Loops/src/com/cg/loops/unreachablestatementexample.java
@@ -0,0 +1,14 @@
+package com.cg.loops;
+
+public class unreachablestatementexample {
+
+	public static void main(String[] args) {
+		while(true)
+		{
+	        System.out.println("C2TC PROGRAM");
+	    }
+
+
+	}
+
+}
