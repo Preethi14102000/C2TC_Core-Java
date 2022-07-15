40d7bb8d3932270a119d214aae2998088d5b3d99
Preethi0112 <106079232+Preethi0112@users.noreply.github.com>
2022-07-15 12:28:03 +0530

    *firstcommit

---
+++ ClassesandObjects/src/com/cg/classesandobjects/Company.java
@@ -0,0 +1,21 @@
+package com.cg.classesandobjects;
+
+public class Company {
+	String name;
+
+	public static void main(String[] args) {
+		
+	    
+	        Company c = new Company();
+	        Company c1 = c;
+	        c1.name ='c2tc';
+	        c = null;
+	        System.out. println(c.name);
+	}
+	        
+	}
+
+
+	
+
+
