dc304dcad06dbaf354987b6ef51a36c3c5f1caf8
Preethi0112 <106079232+Preethi0112@users.noreply.github.com>
2022-08-11 23:39:17 +0530

    *firstcommit

--- Scanner class/src/com/cg/scannerclass/student.java
+++ Scanner class/src/com/cg/scannerclass/student.java
@@ -1,10 +1,27 @@
 package com.cg.scannerclass;
 
+import java.util.Scanner;
+
 public class student {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in); //scanner class object creation
+		String studentid = next();
+		String studentname= sc.nextLine();
+		String next;
+		char gender= next.charAt(0);
+		int age=sc.nextInt();
+		double cgpa = sc.nextDouble();
+		Object system;
+		system.out.println("wsd");
+		
+				
 
+	}
+
+	private static String next() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
---
+++ Strings/src/com/cg/string/basicstringprogram.java
@@ -0,0 +1,14 @@
+package com.cg.string;
+
+public class basicstringprogram {
+
+	public static void main(String[] args) {
+		String str = "Programming";
+        String s = new String("World");
+        System.out.println(str);
+        System.out.println(s);
+
+
+	}
+
+}
